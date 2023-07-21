032e64acd7a86b3ba33f822439a6c68ba2ab631e
Franci <francesca1999@yahoo.com>
2023-07-21 13:31:41 +0300

    finish initial version

---
+++ src/js/UiSlider.js
@@ -0,0 +1,26 @@
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    let uislider = document.getElementById("uislider");
+    let min = document.getElementById("min-slider");
+    let max = document.getElementById("max-slider");
+  
+    noUiSlider.create(uislider, {
+      start: [30, 85], 
+      step: 1,
+      margin: 0,
+      connect: true, 
+      orientation: 'horizontal', // Orient the slider vertically
+      behaviour: 'tap-drag', // Move handle on tap, bar is draggable
+      range: { 
+        min: 15,
+        max: 100
+      },
+    });
+  
+    uislider.noUiSlider.on('update', function (values, handle) {
+      min.textContent = "$" + values[0];
+      max.textContent = "$" + values[1];
+    });
+    
+  });
